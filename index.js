2ac0647e69e97b750785bd4accf1cb737dfa6c41
skallenfreck <fresnedaalexander@gmail.com>
2025-11-02 01:24:05 -0500

    Express
    Se instala Express en el proyecto y se actualiza el index para corroborar su funcionamiento.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-'use strict';
-const http = require('http');
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'content-type': 'text/plain' });
-    res.end('Hola Mundo');
-});
-server.listen(5000);
+const express = require ('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('¡Hola mundo!')
+})
+
+app.listen(port, () => {
+    console.log('La aplicación esta corriendo en el puerto' +  `${port}`)
+})
